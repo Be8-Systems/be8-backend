4ec734f572564e1eb27246b042ca02fa3bba9441
bobo23 <bosobieski@gmail.com>
2022-06-22 12:02:56 +0200

    added setKey fail test

---
+++ test/keys/setKey/fail.mjs
@@ -0,0 +1,19 @@
+import test from 'node:test';
+import assert from 'assert/strict';
+import nodeFetch from 'node-fetch';
+import { baseUrl, newAccOptions, getPostOptions } from '../../utils/utils.mjs';
+
+const accOptions = newAccOptions();
+
+test('SUCCESS setKey', async function () {
+    // new acc
+    const accResponse = await nodeFetch(`${baseUrl()}/newAcc`, accOptions);
+    const cookie = accResponse.headers.get('set-cookie');
+    // setKey
+    const setKeyBody = { };
+    const setKeyOptions = getPostOptions(setKeyBody, cookie);
+    const response = await nodeFetch(`${baseUrl()}/setkey`, setKeyOptions);
+    const data = await response.json();
+    
+    return assert(data.error, 'NOPUBLICKEY');
+});
