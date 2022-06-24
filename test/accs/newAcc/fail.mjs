40bb4de5b9b443247eb227f61daaa725754bf6c7
bobo23 <bosobieski@gmail.com>
2022-06-24 11:50:05 +0200

    fixed error cases and finished authtest

--- test/accs/newAcc/fail.mjs
+++ test/accs/newAcc/fail.mjs
@@ -1,3 +1,22 @@
-// ToDo: insert malformed utf-8 passwords
-// try empty pw
-// fix in code prevent DDOS attack
+import test from 'node:test';
+import assert from 'assert/strict';
+import { baseUrl } from '../../utils/utils.mjs';
+import randomString from '../../utils/randomString.mjs';
+
+const options = {
+    method: 'POST',
+    body: JSON.stringify({
+        password: randomString(5200000), // max length for body parser
+        nickname: randomString(10),
+    }),
+    credentials: 'same-origin',
+    headers: { 'Content-Type': 'application/json' },
+};
+
+test('FAIL newAcc', async function () {
+    const response = await fetch(`${baseUrl()}/newAcc`, options);
+    const data = await response.json();
+    
+    assert.strictEqual(data.valid, false);
+    return assert.strictEqual(data.warning, 'PASSWORDFAILURE');
+});
--- test/generic/authtest.mjs
+++ test/generic/authtest.mjs
@@ -4,22 +4,51 @@ import nodeFetch from 'node-fetch';
 import { baseUrl, newAccOptions, getPostOptions, getGetOptions } from '../utils/utils.mjs';
 import randomString from '../utils/randomString.mjs';
 
-const nickname = randomString(10);
-const accOptions = newAccOptions(nickname);
-const cookie = 'invalid';
-const postOptions = getPostOptions({}, cookie);
-const getOptions = getGetOptions(cookie);
-const routesWithAuth = [
-    [`${baseUrl()}/getmessages`, postOptions],
-    [`${baseUrl()}/me`, getOptions],
-    // ToDo: add all routes
-];
-
 test('FAIL getMessages', async function (context) {
-    await nodeFetch(`${baseUrl()}/newAcc`, accOptions);
+    const accOptions = newAccOptions();
+    const accResponse = await nodeFetch(`${baseUrl()}/newAcc`, accOptions);
+    const acc = await accResponse.json();
+    const validCookie = accResponse.headers.get('set-cookie');
+    const groupBody = {
+        nickname: randomString(7),
+        groupType: 'public',
+    };
+    const groupOptions = getPostOptions(groupBody, validCookie);
+    const groupResponse = await nodeFetch(`${baseUrl()}/groupcreate`, groupOptions);
+    const group = await groupResponse.json();
+    const invalidCookie = 'invalid';
+    const body = {
+        groupID: group.groupID,
+        accID: acc.accID + ''
+    };
+    const postOptions = getPostOptions(body, invalidCookie);
+    const getOptions = getGetOptions(invalidCookie);
+    const routesWithAuth = [
+        [`${baseUrl()}/me`, getOptions],
+        [`${baseUrl()}/changenickname`, postOptions],
+        [`${baseUrl()}/getmessages`, postOptions],
+        [`${baseUrl()}/getthreads`, getOptions],
+        [`${baseUrl()}/startconversation`, postOptions],
+        [`${baseUrl()}/writemessage`, postOptions],
+        [`${baseUrl()}/groupaddmember`, postOptions],
+        [`${baseUrl()}/groupcreate`, postOptions],
+        [`${baseUrl()}/groupgetcurrentversion`, postOptions],
+        [`${baseUrl()}/groupgetkeys`, postOptions],
+        [`${baseUrl()}/groupgetmembers`, postOptions],
+        [`${baseUrl()}/groupjoinmember`, postOptions],
+        [`${baseUrl()}/groupkickmember`, postOptions],
+        [`${baseUrl()}/groupleavemember`, postOptions],
+        [`${baseUrl()}/groupstorekey`, postOptions],
+        [`${baseUrl()}/invitelink`, postOptions],
+        [`${baseUrl()}/getkey`, postOptions],
+        [`${baseUrl()}/getkeys`, postOptions],
+        [`${baseUrl()}/setkey`, postOptions],
+        [`${baseUrl()}/insights`, getOptions],
+        [`${baseUrl()}/subscribe`, postOptions]
+    ];
 
     const tests = await routesWithAuth.map(async function (options) {
-        await context.test(options[1], async () => {
+        await context.test(options[0], async () => {
             const response = await nodeFetch(...options);
             const data = await response.json();
 
