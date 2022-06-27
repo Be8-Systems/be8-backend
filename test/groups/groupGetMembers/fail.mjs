ff04f7198216fff5f6bbe6112e89ee8a4e10b289
bobo23 <bosobieski@gmail.com>
2022-06-27 14:33:40 +0200

    added group get members fail cases and tests

--- test/groups/groupGetMembers/fail.mjs
+++ test/groups/groupGetMembers/fail.mjs
@@ -10,21 +10,21 @@ const firstAccOptions = newAccOptions(nickname1);
 const secondAccOptions = newAccOptions(nickname2);
 const thirdAccOptions = newAccOptions();
 
-test('FAIL groupGetMembers', async function () {
+test('FAIL groupGetMembers', async function (context) {
     // create accs
     const firstAcc = await nodeFetch(`${baseUrl}/newAcc`, firstAccOptions);
     const secondAcc = await nodeFetch(`${baseUrl}/newAcc`, secondAccOptions);
     const firstAccData = await firstAcc.json();
     const secondAccData = await secondAcc.json();
-    const cookie = firstAcc.headers.get('set-cookie');
+    const validCookie = firstAcc.headers.get('set-cookie');
     const thirdAcc = await nodeFetch(`${baseUrl}/newAcc`, thirdAccOptions);
-    const thirdAccCookie = thirdAcc.headers.get('set-cookie');
+    const nonGroupCookie = thirdAcc.headers.get('set-cookie');
     // create group
     const groupBody = {
         nickname: randomString(7),
         groupType: 'public',
     };
-    const groupOptions = getPostOptions(groupBody, cookie);
+    const groupOptions = getPostOptions(groupBody, validCookie);
     const groupResponse = await nodeFetch(`${baseUrl}/groupcreate`, groupOptions);
     const group = await groupResponse.json();
     // add to group
@@ -32,17 +32,54 @@ test('FAIL groupGetMembers', async function () {
         groupID: group.groupID,
         memberID: secondAccData.accID + '',
     };
-    const addOptions = getPostOptions(addBody, cookie);
+    const addOptions = getPostOptions(addBody, validCookie);
     const addResponse = await nodeFetch(`${baseUrl}/groupaddmember`, addOptions);
     const added = await addResponse.json();
-    // get members
-    const membersBody = {
+    // fail get members
+    const failBodies = [{
         groupID: group.groupID,
-    };
-    const membersOptions = getPostOptions(membersBody, thirdAccCookie);
-    const membersResponse = await nodeFetch(`${baseUrl}/groupgetmembers`, membersOptions);
-    const failMembers = await membersResponse.json();
+        nonGroupCookie,
+        expected: 'NOGROUPMEMBER',
+        msg: 'non group member trying to get group members'
+    }, {
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID parameter is missing'
+    }, {
+        groupID: '346384738478',
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID not existing'
+    }, {
+        groupID: '',
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID not existing'
+    }, {
+        groupID: false,
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID not a string'
+    }, {
+        groupID: [],
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID not a string'
+    }, {
+        groupID: {},
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID not a string'
+    }, {
+        groupID: 1234,
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID not a string'
+    }];
+    const tests = await failBodies.map(async function (body) {
+        await context.test(body.msg, async () => {
+            const cookie = body.nonGroupCookie || validCookie;
+            const options = getPostOptions(body, cookie);
+            const response = await nodeFetch(`${baseUrl}/groupgetcurrentversion`, options);
+            const data = await response.json();
+            
+            assert(!data.valid);
+            return assert.strictEqual(data.reason, body.expected);
+        });
+    });
 
-    assert.strictEqual(failMembers.reason, 'NOGROUPMEMBER');
-    return assert(!failMembers.valid);
+    await Promise.all(tests);
 });
