06409791ae60a91098546f27033a77d628816704
bobo23 <bosobieski@gmail.com>
2022-06-27 11:21:46 +0200

    added fail cases for group add member

--- test/groups/groupAddMember/fail.mjs
+++ test/groups/groupAddMember/fail.mjs
@@ -8,7 +8,7 @@ const firstAccOptions = newAccOptions();
 const secondAccOptions = newAccOptions();
 const thirdAccOptions = newAccOptions();
 
-test('FAIL groupAddMember', async function () {
+test('FAIL groupAddMember', async function (context) {
     // create accs
     const firstAcc = await nodeFetch(`${baseUrl}/newAcc`, firstAccOptions);
     const secondAcc = await nodeFetch(`${baseUrl}/newAcc`, secondAccOptions);
@@ -36,38 +36,110 @@ test('FAIL groupAddMember', async function () {
     // fail tests
     const failBodies = [
         {
-            groupID: group.groupID,
-            memberID: '134342534', // not existing acc
-            reason: 'MEMBERNOTEXISTING',
+            groupID: 'g1234563545',
+            memberID: secondAccData.accID + '',
+            expected: 'GROUPNOTEXISTING',
+            msg: 'group id not existing'
         },
         {
-            groupID: 'g1234563545', // not existing group
             memberID: secondAccData.accID + '',
-            reason: 'GROUPNOTEXISTING',
+            expected: 'GROUPNOTEXISTING',
+            msg: 'group parameter is missing'
+        },
+        {
+            groupID: null,
+            memberID: secondAccData.accID + '',
+            expected: 'GROUPNOTEXISTING',
+            msg: 'group id not a string'
+        },
+        {
+            groupID: [],
+            memberID: secondAccData.accID + '',
+            expected: 'GROUPNOTEXISTING',
+            msg: 'group id not a string'
+        },
+        {
+            groupID: {},
+            memberID: secondAccData.accID + '',
+            expected: 'GROUPNOTEXISTING',
+            msg: 'group id not a string'
+        },
+        {
+            groupID: '',
+            memberID: secondAccData.accID + '',
+            expected: 'GROUPNOTEXISTING',
+            msg: 'group id not existing'
+        },
+        {
+            groupID: 123,
+            memberID: secondAccData.accID + '',
+            expected: 'GROUPNOTEXISTING',
+            msg: 'group id not a string'
+        },
+        {
+            groupID: group.groupID,
+            expected: 'MEMBERNOTEXISTING',
+            msg: 'member id param is missing'
+        },
+        {
+            groupID: group.groupID,
+            memberID: '134342534',
+            expected: 'MEMBERNOTEXISTING',
+            msg: 'member id not existing'
+        },
+        {
+            groupID: group.groupID,
+            memberID: '',
+            expected: 'MEMBERNOTEXISTING',
+            msg: 'member id not existing'
+        },
+        {
+            groupID: group.groupID,
+            memberID: [],
+            expected: 'MEMBERNOTEXISTING',
+            msg: 'member id not a string'
+        },
+        {
+            groupID: group.groupID,
+            memberID: {},
+            expected: 'MEMBERNOTEXISTING',
+            msg: 'member id not a string'
+        },
+        {
+            groupID: group.groupID,
+            memberID: 123,
+            expected: 'MEMBERNOTEXISTING',
+            msg: 'member id not a string'
+        },
+        {
+            groupID: group.groupID,
+            memberID: null,
+            expected: 'MEMBERNOTEXISTING',
+            msg: 'member id not a string'
         },
         {
             groupID: group.groupID,
             memberID: secondAccData.accID + '', // already joined
-            reason: 'ISALREADYMEMBER',
+            expected: 'ISALREADYMEMBER',
         },
         {
             groupID: group.groupID,
             memberID: thirdAccData.accID + '', // non admin trying to add
-            reason: 'NOTADMIN',
+            expected: 'NOTADMIN',
+            userCookie
         },
     ];
-    const proms = failBodies.map(function (convBody, i) {
-        const cookie = i === 3 ? userCookie : adminCookie;
-        const addMemberOptions = getPostOptions(convBody, cookie);
-
-        return nodeFetch(`${baseUrl}/groupaddmember`, addMemberOptions);
+    const tests = await failBodies.map(async function (body) {
+        await context.test(body.msg, async () => {
+            const cookie = body.userCookie || adminCookie;
+            const options = getPostOptions(body, cookie);
+            const response = await nodeFetch(`${baseUrl}/groupaddmember`, options);
+            const data = await response.json();
+            
+            assert(!data.valid);
+            return assert.strictEqual(data.reason, body.expected);
+        });
     });
-    const responses = await Promise.all(proms);
 
-    responses.forEach(async function (response, i) {
-        const data = await response.json();
-
-        assert.strictEqual(data.reason, failBodies[i].reason);
-        assert.notStrictEqual(data.valid, true);
-    });
+    await Promise.all(tests);
 });
