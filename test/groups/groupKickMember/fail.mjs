5d71d012433d345d78ed1bcdd611c7e1494a8c63
bobo23 <bosobieski@gmail.com>
2022-06-27 16:39:50 +0200

    added failcases for groupKickMember and tests

--- test/groups/groupKickMember/fail.mjs
+++ test/groups/groupKickMember/fail.mjs
@@ -8,7 +8,7 @@ const firstAccOptions = newAccOptions();
 const secondAccOptions = newAccOptions();
 const thirdAccOptions = newAccOptions();
 
-test('FAIL groupKickMember', async function () {
+test('FAIL groupKickMember', async function (context) {
     // create accs
     const firstAcc = await nodeFetch(`${baseUrl}/newAcc`, firstAccOptions);
     const secondAcc = await nodeFetch(`${baseUrl}/newAcc`, secondAccOptions);
@@ -34,37 +34,111 @@ test('FAIL groupKickMember', async function () {
     const addOptions = getPostOptions(addBody, adminCookie);
     const addResponse = await nodeFetch(`${baseUrl}/groupaddmember`, addOptions);
     const added = await addResponse.json();
-
     // fail tests
     const failBodies = [
         {
             groupID: group.groupID,
             accID: secondAccData.accID + '',
-            reason: 'NOTADMIN', // try to kick without being admin
-        },
-        {
-            groupID: 'g1234563545', // not existing group
-            accID: secondAccData.accID + '',
-            reason: 'GROUPNOTEXISTING',
+            userCookie,
+            expected: 'NOTADMIN',
+            msg: 'trying to kick without being the admin'
         },
         {
             groupID: group.groupID,
-            accID: firstAccData.accID + '', // circular
-            reason: 'CIRCULARKICK',
+            accID: firstAccData.accID + '',
+            expected: 'CIRCULARKICK',
+            msg: 'trying to kick yourself'
+        },
+        {
+            groupID: 'g1234563545',
+            accID: secondAccData.accID + '',
+            expected: 'GROUPNOTEXISTING',
+            msg: 'groupID is not existing'
+        },
+        {
+            groupID: '',
+            accID: secondAccData.accID + '',
+            expected: 'GROUPNOTEXISTING',
+            msg: 'groupID is not existing'
+        },
+        {
+            groupID: true,
+            accID: secondAccData.accID + '',
+            expected: 'GROUPNOTEXISTING',
+            msg: 'groupID is not a string'
+        },
+        {
+            groupID: [],
+            accID: secondAccData.accID + '',
+            expected: 'GROUPNOTEXISTING',
+            msg: 'groupID is not a string'
+        },
+        {
+            groupID: {},
+            accID: secondAccData.accID + '',
+            expected: 'GROUPNOTEXISTING',
+            msg: 'groupID is not a string'
+        },
+        {
+            groupID: 123,
+            accID: secondAccData.accID + '',
+            expected: 'GROUPNOTEXISTING',
+            msg: 'groupID is not a string'
+        },
+        {
+            groupID: group.groupID,
+            accID: thirdAccData.accID + '',
+            expected: 'NOGROUPMEMBER',
+            msg: 'acc is not in group'
+        },
+        {
+            groupID: group.groupID,
+            accID: '1234359458495',
+            expected: 'NOGROUPMEMBER',
+            msg: 'Acc is not in group'
+        },
+        {
+            groupID: group.groupID,
+            accID: '',
+            expected: 'NOGROUPMEMBER',
+            msg: 'Acc is not in group'
+        },
+        {
+            groupID: group.groupID,
+            accID: [],
+            expected: 'NOGROUPMEMBER',
+            msg: 'Acc is not in group'
+        },
+        {
+            groupID: group.groupID,
+            accID: null,
+            expected: 'NOGROUPMEMBER',
+            msg: 'Acc is not in group'
+        },
+        {
+            groupID: group.groupID,
+            accID: {},
+            expected: 'NOGROUPMEMBER',
+            msg: 'Acc is not in group'
+        },
+        {
+            groupID: group.groupID,
+            accID: 123,
+            expected: 'NOGROUPMEMBER',
+            msg: 'Acc is not in group'
         },
     ];
-    const proms = failBodies.map(function (kickBody, i) {
-        const cookie = i === 0 ? userCookie : adminCookie;
-        const addMemberOptions = getPostOptions(kickBody, cookie);
-
-        return nodeFetch(`${baseUrl}/groupkickmember`, addMemberOptions);
+    const tests = await failBodies.map(async function (body) {
+        await context.test(body.msg, async () => {
+            const cookie = body.userCookie || adminCookie;
+            const options = getPostOptions(body, cookie);
+            const response = await nodeFetch(`${baseUrl}/groupkickmember`, options);
+            const data = await response.json();
+            
+            assert(!data.valid);
+            return assert.strictEqual(data.reason, body.expected);
+        });
     });
-    const responses = await Promise.all(proms);
 
-    responses.forEach(async function (response, i) {
-        const data = await response.json();
-
-        assert.strictEqual(data.reason, failBodies[i].reason);
-        assert.notStrictEqual(data.valid, true);
-    });
+    await Promise.all(tests);
 });
