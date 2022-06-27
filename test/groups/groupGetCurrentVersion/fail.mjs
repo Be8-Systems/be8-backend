5b5e6a654b36340c0925e356edf391a5d9f3f84a
bobo23 <bosobieski@gmail.com>
2022-06-27 11:52:17 +0200

    added another fail case for group get current version

--- test/groups/groupGetCurrentVersion/fail.mjs
+++ test/groups/groupGetCurrentVersion/fail.mjs
@@ -7,25 +7,65 @@ import randomString from '../../utils/randomString.mjs';
 const firstAccOptions = newAccOptions();
 const secondAccOptions = newAccOptions();
 
-test('FAIL groupGetCurrentVersion', async function () {
+test('FAIL groupGetCurrentVersion', async function (context) {
     // create accs
     const firstAcc = await nodeFetch(`${baseUrl}/newAcc`, firstAccOptions);
     const secondAcc = await nodeFetch(`${baseUrl}/newAcc`, secondAccOptions);
-    const cookie = firstAcc.headers.get('set-cookie');
+    const validCookie = firstAcc.headers.get('set-cookie');
     const nonGroupCookie = secondAcc.headers.get('set-cookie');
     // create group
     const groupBody = {
         nickname: randomString(7),
         groupType: 'public',
     };
-    const groupOptions = getPostOptions(groupBody, cookie);
+    const groupOptions = getPostOptions(groupBody, validCookie);
     const groupResponse = await nodeFetch(`${baseUrl}/groupcreate`, groupOptions);
     const group = await groupResponse.json();
-    // get current version
-    const versionOptions = getPostOptions({ groupID: group.groupID }, nonGroupCookie);
-    const currentVersionResponse = await nodeFetch(`${baseUrl}/groupgetcurrentversion`, versionOptions);
-    const failVersion = await currentVersionResponse.json();
+    // fail get current version
+    const failBodies = [{
+        groupID: group.groupID,
+        nonGroupCookie,
+        expected: 'NOGROUPMEMBER',
+        msg: 'non group member cannot get current version'
+    }, {
+        expected: 'GROUPNOTEXISTING',
+        msg: 'group id parameter is missing'
+    }, {
+        groupID: '',
+        expected: 'GROUPNOTEXISTING',
+        msg: 'group id not existing'
+    }, {
+        groupID: '123423434532424',
+        expected: 'GROUPNOTEXISTING',
+        msg: 'group id not existing'
+    }, {
+        groupID: false,
+        expected: 'GROUPNOTEXISTING',
+        msg: 'group id not a string'
+    }, {
+        groupID: [],
+        expected: 'GROUPNOTEXISTING',
+        msg: 'group id not a string'
+    }, {
+        groupID: {},
+        expected: 'GROUPNOTEXISTING',
+        msg: 'group id not a string'
+    }, {
+        groupID: 123,
+        expected: 'GROUPNOTEXISTING',
+        msg: 'group id not a string'
+    }];
+    const tests = await failBodies.map(async function (body) {
+        await context.test(body.msg, async () => {
+            const cookie = body.nonGroupCookie || validCookie;
+            const options = getPostOptions(body, cookie);
+            const response = await nodeFetch(`${baseUrl}/groupgetcurrentversion`, options);
+            const data = await response.json();
+            
+            assert(!data.valid);
+            return assert.strictEqual(data.reason, body.expected);
+        });
+    });
 
-    assert.strictEqual(failVersion.reason, 'NOGROUPMEMBER');
-    return assert(!failVersion.valid);
+    await Promise.all(tests);
 });
