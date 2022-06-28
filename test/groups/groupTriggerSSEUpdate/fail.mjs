5dbe083934ecd5d41059ae9f2bc1dc0311a9f49c
bobo23 <bosobieski@gmail.com>
2022-06-28 15:00:15 +0200

    added group trigger sse update fail cases and tests

---
+++ test/groups/groupTriggerSSEUpdate/fail.mjs
@@ -0,0 +1,112 @@
+import test from 'node:test';
+import assert from 'assert/strict';
+import nodeFetch from 'node-fetch';
+import { baseUrl, newAccOptions, getPostOptions } from '../../utils/utils.mjs';
+import randomString from '../../utils/randomString.mjs';
+
+const firstAccOptions = newAccOptions();
+const secondAccOptions = newAccOptions();
+const firstAcc = await nodeFetch(`${baseUrl}/newAcc`, firstAccOptions);
+const secondAcc = await nodeFetch(`${baseUrl}/newAcc`, secondAccOptions);
+const validCookie = firstAcc.headers.get('set-cookie');
+const nonGroupCookie = secondAcc.headers.get('set-cookie');
+
+test('FAIL groupTriggerSSEUpdate', async function (context) {
+    // group create
+    const groupBody = {
+        nickname: randomString(7),
+        groupType: 'public',
+    };
+    const groupOptions = getPostOptions(groupBody, validCookie);
+    const groupResponse = await nodeFetch(`${baseUrl}/groupcreate`, groupOptions);
+    const group = await groupResponse.json();
+    // group trigger sse update
+    const failBodies = [{
+        groupID: group.groupID,
+        update: 'joinmember',
+        nonGroupCookie,
+        expected: 'NOGROUPMEMBER',
+        msg: 'acc is not member of the group'
+    }, {
+        update: 'joinmember',
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID paremeter is missing'
+    }, {
+        groupID: 'g2842387238723',
+        update: 'joinmember',
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID is not existing'
+    }, {
+        groupID: '',
+        update: 'joinmember',
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID is not existing'
+    }, {
+        groupID: false,
+        update: 'joinmember',
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID is not a string'
+    }, {
+        groupID: [],
+        update: 'joinmember',
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID is not a string'
+    }, {
+        groupID: {},
+        update: 'joinmember',
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID is not a string'
+    }, {
+        groupID: 1234,
+        update: 'joinmember',
+        expected: 'GROUPNOTEXISTING',
+        msg: 'groupID is not a string'
+    }, {
+        groupID: group.groupID,
+        expected: 'INVALIDUPDATETYPE',
+        msg: 'update parameter is missing'
+    }, {
+        groupID: group.groupID,
+        update: 'notvalid',
+        expected: 'INVALIDUPDATETYPE',
+        msg: 'update is not valid'
+    }, {
+        groupID: group.groupID,
+        update: '',
+        expected: 'INVALIDUPDATETYPE',
+        msg: 'update is not valid'
+    }, {
+        groupID: group.groupID,
+        update: true,
+        expected: 'INVALIDUPDATETYPE',
+        msg: 'update is not a string'
+    }, {
+        groupID: group.groupID,
+        update: [],
+        expected: 'INVALIDUPDATETYPE',
+        msg: 'update is not a string'
+    }, {
+        groupID: group.groupID,
+        update: {},
+        expected: 'INVALIDUPDATETYPE',
+        msg: 'update is not a string'
+    }, {
+        groupID: group.groupID,
+        update: 1234,
+        expected: 'INVALIDUPDATETYPE',
+        msg: 'update is not a string'
+    }];
+    const tests = await failBodies.map(async function (body) {
+        await context.test(body.msg, async () => {
+            const cookie = body.nonGroupCookie || validCookie;
+            const options = getPostOptions(body, cookie);
+            const response = await nodeFetch(`${baseUrl}/grouptriggersseupdate`, options);
+            const data = await response.json();
+            
+            assert(!data.valid);
+            return assert.strictEqual(data.reason, body.expected);
+        });
+    });
+
+    await Promise.all(tests);
+});
---
+++ test/groups/groupTriggerSSEUpdate/success.mjs
@@ -0,0 +1,30 @@
+import test from 'node:test';
+import assert from 'assert/strict';
+import nodeFetch from 'node-fetch';
+import { baseUrl, newAccOptions, getPostOptions } from '../../utils/utils.mjs';
+import randomString from '../../utils/randomString.mjs';
+
+const accOptions = newAccOptions();
+const accResponse = await nodeFetch(`${baseUrl}/newAcc`, accOptions);
+const cookie = accResponse.headers.get('set-cookie');
+
+test('SUCCESS groupTriggerSSEUpdate', async function () {
+    // group create
+    const groupBody = {
+        nickname: randomString(7),
+        groupType: 'public',
+    };
+    const groupOptions = getPostOptions(groupBody, cookie);
+    const groupResponse = await nodeFetch(`${baseUrl}/groupcreate`, groupOptions);
+    const group = await groupResponse.json();
+    // group trigger sse update
+    const updateBody = {
+        groupID: group.groupID,
+        update: 'joinmember'
+    };
+    const updateOptions = getPostOptions(updateBody, cookie);
+    const updateResponse = await nodeFetch(`${baseUrl}/grouptriggersseupdate`, updateOptions);
+    const update = await updateResponse.json();
+  
+    return assert(update.valid);
+});
