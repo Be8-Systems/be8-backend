0994a49f5a82e80279d43b43b1b86df229685d3b
bobo23 <bosobieski@gmail.com>
2022-07-07 16:32:21 +0200

    promo now added to token

--- index.mjs
+++ index.mjs
@@ -139,7 +139,7 @@ export default function start ({ fakeTokens = [], staticFiles = './node_modules/
     if (fakeTokens.length > 0) {
         const proms = fakeTokens.map(function ({ token, type, validTime }) {
             const basic = { active: false, type };
-            const options = token.promo ? { ...basic, validTime } : basic;
+            const options = token.validTime ? { ...basic, validTime } : basic;
 
             redis.hSet(`token:${token}`, options);
         });
