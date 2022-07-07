eefef48e2edc10aa21a4785719aac44ac36a21ca
bobo23 <bosobieski@gmail.com>
2022-07-07 11:50:08 +0200

    fixed tests due to oldCode at code uptade

--- test/code/codeUpdate/fail.mjs
+++ test/code/codeUpdate/fail.mjs
@@ -18,67 +18,115 @@ test('FAIL update code', async function (context) {
     await nodeFetch(`${baseUrl}/codeset`, codeSetOptions);
     const failBodies = [{
         codeType: 'unlock',
+        oldCode: codeBody.unlockCode,
         expected: 'INVALIDCODE',
         msg: 'code parameter is missing'
     }, {
         code: '',
         codeType: 'destroy',
+        oldCode: codeBody.destroyCode,
         expected: 'INVALIDCODE',
         msg: 'code is too short'
     }, {
         code: null,
         codeType: 'destroy',
+        oldCode: codeBody.destroyCode,
         expected: 'INVALIDCODE',
         msg: 'code is not a string'
     }, {
         code: {},
         codeType: 'unlock',
+        oldCode: codeBody.unlockCode,
         expected: 'INVALIDCODE',
         msg: 'code is not a string'
     }, {
         code: [],
         codeType: 'unlock',
+        oldCode: codeBody.unlockCode,
         expected: 'INVALIDCODE',
         msg: 'code is not a string'
     }, {
         code: 1234,
         codeType: 'destroy',
+        oldCode: codeBody.destroyCode,
         expected: 'INVALIDCODE',
         msg: 'code is not a string'
     }, {
         code: randomString(9),
+        oldCode: codeBody.unlockCode,
         expected: 'INVALIDCODETYPE',
         msg: 'codeType parameter is missing'
     }, {
         code: randomString(9),
         codeType: 'invalid',
+        oldCode: codeBody.unlockCode,
         expected: 'INVALIDCODETYPE',
         msg: 'codeType is not valid'
     }, {
         code: randomString(9),
         codeType: '',
+        oldCode: codeBody.unlockCode,
         expected: 'INVALIDCODETYPE',
         msg: 'codeType is not valid'
     }, {
         code: randomString(9),
         codeType: null,
+        oldCode: codeBody.unlockCode,
         expected: 'INVALIDCODETYPE',
         msg: 'codeType is not a string'
     }, {
         code: randomString(9),
         codeType: {},
+        oldCode: codeBody.unlockCode,
         expected: 'INVALIDCODETYPE',
         msg: 'codeType is not a string'
     }, {
         code: randomString(9),
         codeType: [],
+        oldCode: codeBody.unlockCode,
         expected: 'INVALIDCODETYPE',
         msg: 'codeType is not a string'
     }, {
         code: randomString(9),
         codeType: 1234,
+        oldCode: codeBody.unlockCode,
         expected: 'INVALIDCODETYPE',
         msg: 'codeType is not a string'
+    }, {
+        code: randomString(9),
+        codeType: 'unlock',
+        expected: 'INVALIDOLDCODE',
+        msg: 'oldCode parameter is missing'
+    }, {
+        code: randomString(9),
+        codeType: 'unlock',
+        oldCode: '',
+        expected: 'INVALIDOLDCODE',
+        msg: 'oldCode parameter is invalid'
+    }, {
+        code: randomString(9),
+        codeType: 'unlock',
+        oldCode: false,
+        expected: 'INVALIDOLDCODE',
+        msg: 'oldCode parameter is not a string'
+    }, {
+        code: randomString(9),
+        codeType: 'unlock',
+        oldCode: [],
+        expected: 'INVALIDOLDCODE',
+        msg: 'oldCode parameter is not a string'
+    }, {
+        code: randomString(9),
+        codeType: 'unlock',
+        oldCode: {},
+        expected: 'INVALIDOLDCODE',
+        msg: 'oldCode parameter is not a string'
+    }, {
+        code: randomString(9),
+        codeType: 'unlock',
+        oldCode: 1234,
+        expected: 'INVALIDOLDCODE',
+        msg: 'oldCode parameter is not a string'
     }];
     const tests = await failBodies.map(async function (body) {
         await context.test(body.msg, async () => {
